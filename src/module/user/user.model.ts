2fe25ed00144fa900367f5f2c713dcbff35c5691
Mehebul02 <mehebul2123@gmail.com>
2024-11-22 22:39:59 +0600

    added the validate email

--- src/module/user/user.model.ts
+++ src/module/user/user.model.ts
@@ -4,12 +4,18 @@ import { model, Schema } from "mongoose";
 const userSchema = new Schema({
     name: { type: String, required: true },
     age: { type: Number, required: [true, "Please enter your age"] },
-    email: { type: String, required: [true,"Please enter your email"] ,
+    email: {
+        type: String, required: [true, "Please enter your email"], unique: true,
+        validate: {
+            validator: function (value) {
+                return /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(value)
+            }
+        }
 
     },
     photo: { type: String, required: true },
-    role: { type: String,enum:["user", 'admin'], required: true },
-    userStatus: { type: String,enum:{values:["active", "inactive"],message:"{VALUES} is not valid"}, required: true },
+    role: { type: String, enum: ["user", 'admin'], required: true },
+    userStatus: { type: String, enum: { values: ["active", "inactive"], message: "{VALUES} is not valid" }, required: true },
 })
 
 
