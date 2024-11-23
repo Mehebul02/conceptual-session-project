473a683c81cb8bdefcc568c6a15e91052510b59d
Mehebul02 <mehebul2123@gmail.com>
2024-11-23 23:02:18 +0600

    added the partial code

---
+++ src/module/user/tour.controller.ts
@@ -0,0 +1,23 @@
+import { ITour } from "./tour.interface";
+import Tour from "./tour.model";
+
+
+const createTours = async (payload: ITour) => {
+    const result = await Tour.create(payload)
+    return result
+}
+
+const getTours = async () => {
+    const result = await Tour.find()
+    return result
+}
+
+const getSingleTour = async (id: string) => {
+    const result = await Tour.findById(id)
+    return result
+}
+
+const updateTour = async (id: string, payload: Partial<ITour>) => {
+    const result = await Tour.findByIdAndUpdate(id, payload)
+    return result
+}
